9f7a3407cc5e07824e49cc6c4b2ff0e4fdae126f
felipe <marcelus20felipe@gmail.com>
2018-05-24 12:02:30 +0100

    Added 1 getter to the TUple Of 3 class

--- src/com/company/TupleOf3.java
+++ src/com/company/TupleOf3.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class TupleOf3 <B> extends Tupleof2 {
-    private final B _3;
+    protected final B _3;
 
     protected <T, G, A> TupleOf3(T _1, G _2, B _3) {
         super(_1, _2);
@@ -17,6 +17,10 @@ public class TupleOf3 <B> extends Tupleof2 {
         T _1 = (T) tupleOf2.get_1();
         G _2 = (G) tupleOf2.get_2();
         return new TupleOf3(_1, _2, _3);
+    }
+
+    public B get_3() {
+        return _3;
     }
 
     @Override
