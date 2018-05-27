89c08dd73871c4c2db32a20c6df15c18fbdc0bcd
felipe <marcelus20felipe@gmail.com>
2018-05-27 16:04:33 +0100

    changed Object type in parameter to Generics

--- src/com/company/tuples/TupleOf2.java
+++ src/com/company/tuples/TupleOf2.java
@@ -1,8 +1,8 @@
 package com.company.tuples;
 
-public class TupleOf2 extends Tuple {
+public class TupleOf2 <T, G> extends Tuple  {
 
-    protected TupleOf2(Object _1, Object _2) {
+    protected TupleOf2(T _1, G _2) {
         super(_1, _2);
     }
 
