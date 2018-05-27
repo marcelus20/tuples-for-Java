64860ae296e500ce372d22ec64d897853cf93e69
felipe <marcelus20felipe@gmail.com>
2018-05-27 16:12:31 +0100

    Changed the return type of the static tupleOf2 method

--- src/com/company/tuples/TupleOf2.java
+++ src/com/company/tuples/TupleOf2.java
@@ -6,7 +6,7 @@ public class TupleOf2 <T, G> extends Tuple  {
         super(_1, _2);
     }
 
-    public static<T, G> Tuple tupleOf2( T _1, G _2){
+    public static<T, G> TupleOf2 tupleOf2( T _1, G _2){
         return new TupleOf2( _1, _2);
     }
 
